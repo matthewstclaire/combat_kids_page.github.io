366d9a97043ad5d74f8c8ef32bb4c09545bbbebd
Matthew St Claire <matthew.stclaire@mx.com>
2022-08-24 17:19:25 -0600

    added data file with data for mapping in Helpful Links

---
+++ data/data.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    id: 1,
+    title: "A brief history of Jiu-Jitsu",
+    link: "https://www.greenwichjiujitsu.com/history-of-jiu-jitsu",
+    buttonText: "History Lesson",
+  },
+  {
+    id: 2,
+    title: "Jiu-Jitsu at the highest of levels",
+    link: "https://www.youtube.com/watch?v=XcI8ct3Ifik",
+    buttonText: "Watch It Here",
+  },
+  {
+    id: 3,
+    title:
+      "IBJJF Graduation System will explain in depth what promotions entail.",
+    link: "https://ibjjf.com/graduation-system",
+    buttonText: "Graduation System",
+  },
+  {
+    id: 4,
+    title:
+      "The IBJJF Rule Book will give you detailed explanation of the rules. They can be complicated!",
+    link: "https://ibjjf.com/books-videos",
+    buttonText: "IBJJF Rulebook",
+  },
+  {
+    id: 5,
+    title:
+      "This article was written by a woman starting Jiu-Jitsu later in life.",
+    link: "https://www.buzzfeed.com/nataliakrslovic/taking-up-brazilian-jiu-jitsu",
+    buttonText: "Her Story Here",
+  },
+  {
+    id: 6,
+    title:
+      "This is an article written by Yahoo discussing reasons why Jiu-Jitsu is great for kids.",
+    link: "https://sg.news.yahoo.com/4-reasons-why-brazilian-jiu-jitsu-is-one-of-the-best-martial-arts-for-kids-013025041.html?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAJ4TsPkJT0aDv6mUDFs5fTebS9EwaYjBLc_-d_QLYeHxjrXzmiDQIQT2-7wIBkO9E4bRZSKqmRAaqdKRZkuym5XD4sKRq815wbhq87ja3SEbtafNzRShTyCCvzU3oUTyudrFj9vSEMiArtyoR3k8l8gRPbpws6rwkJZ9jZbzO_yI",
+    buttonText: "Benefits for Kids",
+  },
+];
